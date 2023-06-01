fb2f51323f58bfe4be178c5479f19be63c0f8c55
JMarioGilD <jmariogild88@gmail.com>
2023-06-01 14:40:42 -0500

    agregando informacion del componente header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Header from './Components/Header'
 
 export default function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            <h1>Lista de tareas de la semana</h1>
+        </div>
+    )
+}
+
+export default Header;
