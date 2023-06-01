c72d1fb7fedc1e87dc04904dc706b913950f8322
JMarioGilD <jmariogild88@gmail.com>
2023-06-01 15:28:25 -0500

    agregando informacion al componente tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Header from './Components/Header'
+import TaskList from './Components/TaskList'
 
 export default function App() {
 
   return (
     <div>
       <Header />
+      <TaskList />
     </div>
   )
 }
---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,14 @@
+function TaskList() {
+    return (
+        <div>
+            <ul>
+              <li>Limpiar el polvo</li>
+              <li>Barrer</li>
+              <li>Pasar la aspiradora</li>
+              <li>Trapear</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TaskList;
